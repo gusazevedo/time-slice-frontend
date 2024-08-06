dee2381734ebf9d43cd383fb447cb488ed16a3db
Gustavo Azevedo <gustavoazevedogan@gmail.com>
2024-08-06 18:39:15 -0300

    [FEATURE] - Setup tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,3 @@
 export function App() {
-  return (
-    <section>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt et, ab
-        fugit similique iste est totam distinctio error quisquam sit eligendi,
-        voluptatem dolores qui, odit vitae reiciendis aut quibusdam quaerat.
-      </p>
-    </section>
-  )
+  return <h1 className="font-sans text-3xl font-bold">Hello world!</h1>
 }
