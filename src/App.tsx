20febe72ecba3f844de8358b2285c97e3868f1b6
Gustavo Azevedo <gustavoazevedogan@gmail.com>
2024-08-06 18:25:53 -0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+export function App() {
+  return (
+    <section>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt et, ab
+        fugit similique iste est totam distinctio error quisquam sit eligendi,
+        voluptatem dolores qui, odit vitae reiciendis aut quibusdam quaerat.
+      </p>
+    </section>
+  )
+}
